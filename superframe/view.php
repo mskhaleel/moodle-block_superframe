97a3a72fa5d01a50d6ae65b11759fe0969c55f2c
mohammad alkhalil <38015934+mskhaleel@users.noreply.github.com>
2019-07-23 13:18:30 +0300

    Update view.php
    updating view.php

--- superframe/view.php
+++ superframe/view.php
@@ -21,14 +21,61 @@
  * Modified for use in MoodleBites for Developers Level 1 by Richard Jones & Justin Hunt
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+
+
 require('../../config.php');
+$blockid = required_param('blockid', PARAM_INT);
+$def_config = get_config('block_superframe');
 $PAGE->set_course($COURSE);
 $PAGE->set_url('/blocks/superframe/view.php');
 $PAGE->set_heading($SITE->fullname);
-$PAGE->set_pagelayout('course');
+$PAGE->set_pagelayout($def_config->pagelayout);
 $PAGE->set_title(get_string('pluginname', 'block_superframe'));
 $PAGE->navbar->add(get_string('pluginname', 'block_superframe'));
 require_login();
+
+//$block = $DB->get_record('block_instance', array('id' => $blockid), '*', MUST_EXIST);
+$context = context_block::instance($blockid);
+require_capability('block/superframe:viewlink', $context);
+
+// Get the instance configuration data from the database.
+// It's stored as a base 64 encoded serialized string.
+$configdata = $DB->get_field('block_instances', 'configdata', ['id' => $blockid]);
+
+// If an entry exists, convert to an object.
+if ($configdata) {
+    $config = unserialize(base64_decode($configdata));
+} else {
+    // No instance data, use admin settings.
+    // However, that only specifies height and width, not size.
+   $config = $def_config;
+   $config->size = 'custom';
+}
+
+// URL - comes either from instance or admin.
+$url = $config->url;
+// Let's set up the iframe attributes.
+switch ($config->size) {
+    case 'custom':
+        $width = $def_config->width;
+        $height = $def_config->height;
+        break;
+    case 'small' :
+        $width = 360;
+        $height = 240;
+        break;
+    case 'medium' :
+        $width = 600;
+        $height = 400;
+        break;
+    case 'large' :
+        $width = 1024;
+        $height = 720;
+        break;
+}
+
+
+
 
 // Start output to browser.
 echo $OUTPUT->header();
@@ -42,9 +89,9 @@ $url = 'https://quizlet.com/132695231/scatter/embed';
 $width = '600px';
 $height = '400px';
 
-$attributes = ['src' => $url,
-               'width' => $width,
-               'height' => $height];
+$attributes = ['src' => $config->url,
+               'width' => $config->width,
+               'height' => $config->height];
 echo html_writer::start_tag('iframe', $attributes);
 echo html_writer::end_tag('iframe');
 
